0002d1c867072cfcac4ece719773044a29b60fa4
Elodie S <elodie.sevestre86@gmail.com>
2026-06-30 11:51:23 +0200

    feat: ajout middleware tasksRoutes

--- back/src/server.js
+++ back/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import usersRoutes from "./routes/usersRoutes.js";
+import tasksRoutes from "./routes/tasksRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", usersRoutes);
+app.use("/api/tasks", tasksRoutes);
 
 app.listen(PORT, () => {
   console.log(`Backend running on http://localhost:${PORT}`);
