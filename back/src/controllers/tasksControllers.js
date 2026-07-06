b05018a00f535f204bafccc6b58339e5fd31dd77
Gaed <oger.gaedic@gmail.com>
2026-07-06 11:50:10 +0200

    Ajout de commentaires routes

--- back/src/controllers/tasksControllers.js
+++ back/src/controllers/tasksControllers.js
@@ -1,6 +1,6 @@
 import { getAllTasksService } from "../services/tasksServices.js";
 
-//Le controller contrôle les requête et les réponses:
+//Le controller contrôle les requête et les réponses: (Bon format? Est-ce que j'ai les bonnes infos, au bon format pour ma BDD)
 async function getAllTasksController(req, res) {
   try {
     const tasks = await getAllTasksService();
--- back/src/routes/tasksRoutes.js
+++ back/src/routes/tasksRoutes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { getAllTasksController } from "../controllers/tasksControllers.js";
 
-//Aiguilleur, le router ici aiguille vers les bonnes routes:
+//Aiguilleur, le router ici aiguille vers les bonnes routes: "Ecoute ce type de requêtes"
 const tasksRoutes = Router();
 
-tasksRoutes.get("/", getAllTasksController); // Ici la routepour aller consulter toutes les tâches (Vue Bernard)
+tasksRoutes.get("/", getAllTasksController); // Ici la route pour aller consulter toutes les tâches (Vue Bernard)
 
 export default tasksRoutes;
