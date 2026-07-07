ddf44751cba1f816f805aa18c223d4011cfbcb56
Elodie S <elodie.sevestre86@gmail.com>
2026-07-07 07:08:47 +0200

    feat: construction des routes PATCH pour la modif des tâches

--- back/src/routes/tasksRoutes.js
+++ back/src/routes/tasksRoutes.js
@@ -3,21 +3,18 @@
 import { Router } from "express";
 import {
   updateTaskDetailsController,
-  updateTaskStatusController,
   updateTaskUserAssignedController,
 } from "../controllers/tasksControllers.js";
 
 const tasksRoutes = Router();
 
-// Modification tâche : trois types de modifs
-// modif des champs : name, description et points
-// modif du statut
+// Modification tâche : deux types de modifs
+// modif des champs : name, description, status, points
 // modif de l'assignation
 // Methode HTTP : utilisation de PATCH car modif partielle
 // modif via l'id
 
 tasksRoutes.patch("/:id", updateTaskDetailsController);
-tasksRoutes.patch("/:id/status", updateTaskStatusController);
 tasksRoutes.patch("/:id/assigned-user", updateTaskUserAssignedController);
 
 export default tasksRoutes;
