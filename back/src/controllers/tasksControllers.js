72d63ecfaebaa06f9855c2fb33dc8d40c8d33742
Gaed <oger.gaedic@gmail.com>
2026-07-07 16:22:27 +0200

    Ajout du controller en try and catch + service sépare les taches

--- back/src/controllers/tasksControllers.js
+++ back/src/controllers/tasksControllers.js
@@ -4,9 +4,11 @@ import { getAllTasksService } from "../services/tasksServices.js";
 async function getAllTasksController(req, res) {
   try {
     const tasks = await getAllTasksService();
-    res.json(tasks);
+    res.status(200).json(tasks);
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    res
+      .status(500)
+      .json({ error: "Erreur lors de la récupération des tâches" });
   }
 }
 
--- back/src/services/tasksServices.js
+++ back/src/services/tasksServices.js
@@ -2,7 +2,11 @@ import { getAllTasksModel } from "../models/tasksModels.js";
 
 async function getAllTasksService() {
   const tasks = await getAllTasksModel();
-  return tasks;
+  // Je veux stocker les tâhces à faire dans un nouveau tableau
+  const todoTasks = tasks.filter((tasks) => task.status === "A_FAIRE");
+  // Je veux stocker les tâches terminées dans un nouveau tableau
+  const finishedTasks = tasks.filter((tasks) => task.status === "TERMINES");
+  return { todoTasks, finishedTasks };
 }
 
 export { getAllTasksService };
