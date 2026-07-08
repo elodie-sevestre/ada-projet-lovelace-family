79a7ed4c0cbda7c7b06605269d9ce83c4e3e7eca
Gaed <oger.gaedic@gmail.com>
2026-07-08 15:38:32 +0200

    feature: routes get task ajout commentaires

--- back/src/controllers/tasksControllers.js
+++ back/src/controllers/tasksControllers.js
@@ -20,7 +20,7 @@ async function getTasksByUserController(req, res) {
   //Validation : Vérifier que mon id est bien un nombre: Question de sécurité ?
   // if(id is not a number) {return error 400 blabliblou}
   try {
-    const tasksByUser = await getTasksByUserService(id);
+    const tasksByUser = await getTasksByUserService(id); //Ne pas oulbier de passer l'id en paramètre
     res.status(200).json(tasksByUser);
   } catch (err) {
     res.status(500).json({
