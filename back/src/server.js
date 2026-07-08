69ec3454ac7eb39425415c02d448ed7b6587ac85
Offichigo <oph.perianin@gmail.com>
2026-07-08 15:20:22 +0200

    feature: route operationnel

--- back/src/server.js
+++ back/src/server.js
@@ -4,7 +4,7 @@ import usersRoutes from "./routes/usersRoutes.js";
 import tasksRoutes from "./routes/tasksRoutes.js";
 
 const app = express();
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
