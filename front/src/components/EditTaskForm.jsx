90eafd2b567046bac8c56927243566689fe0fa50
Elodie S <elodie.sevestre86@gmail.com>
2026-07-14 18:52:43 +0200

    feat: Formulaire de modification s'affiche correctement

--- front/src/components/EditTaskForm.jsx
+++ front/src/components/EditTaskForm.jsx
@@ -9,7 +9,7 @@
 // sur le bouton enregistrer -> modification de la bdd (ajouter un toast de qqs secondes pour pouvoir annuler si clique par erreur)
 // sur le bouton annuler
 
-// bouton modifier dans adahsboard
+// exemple : bouton modifier dans adahsboard
 
 /* CategoryCard.jsx
 
@@ -51,3 +51,83 @@ const [editName, setEditName] = useState(category.name);
         ></button>
 
 */
+
+import { useState } from "react";
+
+const EditTaskForm = ({ task }) => {
+  const [editName, setEditName] = useState(task.task_name);
+  const [editDescription, setEditDescription] = useState(task.description);
+  const [editPoints, setEditPoints] = useState(task.points);
+  const [editStatus, setEditStatus] = useState(task.status);
+  const [editUserId, setEditUserId] = useState(task.assigned_user_ids[0]);
+
+  const usersName = [
+    { id: 1, name: "Bernard" },
+    { id: 2, name: "Léa" },
+  ];
+  const usersList = usersName.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
+
+  return (
+    <>
+      <label>
+        {" "}
+        Nom de la tâche
+        <input
+          type="text"
+          className="task-name"
+          value={editName}
+          onChange={(event) => setEditName(event.target.value)}
+        />
+      </label>
+      <label>
+        {" "}
+        Description de la tâche
+        <textarea
+          maxLength={255}
+          className="task-description"
+          value={editDescription}
+          onChange={(event) => setEditDescription(event.target.value)}
+        />
+      </label>
+      <label>
+        {" "}
+        Nombre de points attribués
+        <input
+          type="number"
+          className="task-points"
+          value={editPoints}
+          onChange={(event) => setEditPoints(event.target.valueAsNumber)}
+        />
+      </label>
+      <label>
+        {" "}
+        Statut de la tâche
+        <select
+          className="task-status"
+          value={editStatus}
+          onChange={(event) => setEditStatus(event.target.value)}
+        >
+          <option value={"A_FAIRE"}>A faire</option>
+          <option value={"TERMINE"}>Terminée</option>
+        </select>
+      </label>
+      <label>
+        {" "}
+        Membre assigné
+        <select
+          className="user-id"
+          value={editUserId}
+          onChange={(event) => setEditUserId(Number(event.target.value))}
+        >
+          {usersList}
+        </select>
+      </label>
+    </>
+  );
+};
+
+export default EditTaskForm;
