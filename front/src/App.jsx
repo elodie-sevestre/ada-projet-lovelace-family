d8ac820a0a0c6308c29f2df3c8d05caefc9af0d3
Elodie S <elodie.sevestre86@gmail.com>
2026-07-14 18:53:15 +0200

    feat: test du bouton modifier une tâche avec un objet en dur

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,7 +1,12 @@
-import TaskItem from "./TaskItem.jsx";
+import { useState } from "react";
+import TaskItem from "./components/TaskItem.jsx";
+import EditTaskButton from "./components/EditTaskButton.jsx";
+import EditTaskForm from "./components/EditTaskForm.jsx";
 import "./App.css";
 
 function App() {
+  const [isEditOpen, setIsEditOpen] = useState(false);
+
   return (
     <>
       <section id="center">
@@ -13,6 +18,29 @@ function App() {
           }}
           currentUser={{ role: "ADMIN" }}
         ></TaskItem>
+        <EditTaskButton
+          task={{
+            task_name: "Faire la vaisselle",
+            description:
+              "Mettre une pastille dans le réservoir du lave vaisselle et lancer un cycle en mode eco",
+            points: 5,
+            status: "A_FAIRE",
+            assigned_user_ids: [0],
+          }}
+          onEdit={() => setIsEditOpen(true)}
+        ></EditTaskButton>
+        {isEditOpen && (
+          <EditTaskForm
+            task={{
+              task_name: "Faire la vaisselle",
+              description:
+                "Mettre une pastille dans le réservoir du lave vaisselle et lancer un cycle en mode eco",
+              points: 5,
+              status: "A_FAIRE",
+              assigned_user_ids: [0],
+            }}
+          />
+        )}
       </section>
     </>
   );
