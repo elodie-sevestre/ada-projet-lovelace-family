d335d38bc0d3a817a1bf5061dfbf37fc9958374c
Elodie S <elodie.sevestre86@gmail.com>
2026-07-14 23:19:54 +0200

    test: enrichir l'objet tâche simulé dans App.jsx pour couvrir les champs d'édition

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,7 @@
 import TaskItem from "./components/TaskItem.jsx";
 import "./App.css";
 
-// import pour simuler modification tâche
-import { useState } from "react";
-import EditTaskButton from "./components/EditTaskButton.jsx";
-import EditTaskForm from "./components/EditTaskForm.jsx";
-
 function App() {
-  // useState pour éditer formulaire de modification
-  const [isEditOpen, setIsEditOpen] = useState(false);
-
   return (
     <>
       <section id="center">
@@ -18,36 +10,14 @@ function App() {
             task_name: "Faire la vaisselle",
             assigned_to: "Léa",
             points: 10,
+            description:
+              "Mettre une pastille dans le réservoir du lave vaisselle et lancer un cycle en mode eco",
+            status: "A_FAIRE",
+            // valeur 2 correspond à l'id 2 du seed de la table users
+            assigned_user_ids: [2],
           }}
           currentUser={{ role: "ADMIN" }}
         ></TaskItem>
-
-        {/* Simulation du comportoment du bouton modifier */}
-        <EditTaskButton
-          task={{
-            task_name: "Faire la vaisselle",
-            description:
-              "Mettre une pastille dans le réservoir du lave vaisselle et lancer un cycle en mode eco",
-            points: 5,
-            status: "A_FAIRE",
-            assigned_user_ids: [0],
-          }}
-          // props onEdit permet de mettre à jour le statut d'affichage du formulaire qd on clique
-          onEdit={() => setIsEditOpen(true)}
-        ></EditTaskButton>
-        {/* simulation pour masquer au départ le formulaire de modification */}
-        {isEditOpen && (
-          <EditTaskForm
-            task={{
-              task_name: "Faire la vaisselle",
-              description:
-                "Mettre une pastille dans le réservoir du lave vaisselle et lancer un cycle en mode eco",
-              points: 5,
-              status: "A_FAIRE",
-              assigned_user_ids: [0],
-            }}
-          />
-        )}
       </section>
     </>
   );
