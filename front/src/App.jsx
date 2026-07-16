34ad215836e140da29f6b9a7b5509822ac474d0f
Offichigo <oph.perianin@gmail.com>
2026-07-16 09:29:20 +0200

    fix: delete commentaire dans app

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -17,23 +17,7 @@ function App() {
   return (
     <>
       <section id="center">
-        {/* <TaskItem
-          task={{
-            id: 3,
-            task_name: "Faire la vaisselle",
-            assigned_to: "Léa",
-            points: 10,
-            description:
-              "Mettre une pastille dans le réservoir du lave vaisselle et lancer un cycle en mode eco",
-            status: "A_FAIRE",
-            // valeur 2 correspond à l'id 2 du seed de la table users
-            assigned_user_ids: [2],
-          }}
-          currentUser={{ role: "ADMIN" }}
-        ></TaskItem>
         <TaskForm onCreate={createTask} members={members} />
-          currentUser={{ role: "ADMIN" }} */}
-        {/* /> */}
         <TasksConsultation />
       </section>
     </>
