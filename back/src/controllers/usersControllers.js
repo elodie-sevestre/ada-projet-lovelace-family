6d8e0f1960f7155bbdd5a73a714329a50f79b109
Offichigo <oph.perianin@gmail.com>
2026-07-17 19:39:36 +0200

    feat: back user controller get all user

--- back/src/controllers/usersControllers.js
+++ back/src/controllers/usersControllers.js
@@ -1,8 +1,11 @@
-import getAllUsersService from "./usersServices.js";
+import getAllUsersService from "../services/usersServices.js";
 
-function getAllUsersController(req, res) {
-  const usersResult = getAllUsersService();
-  res.json(usersResult);
+async function getAllUsersController(req, res) {
+  try {
+    const users = await getAllUsersService();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ error: "Erreur lors de la récupération des users" });
+  }
 }
-
 export default getAllUsersController;
