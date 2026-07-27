476ca7ad617669009bf2a5ccf8208e541dd61864
Offichigo <oph.perianin@gmail.com>
2026-07-27 14:07:12 +0200

    fix : front affiche tout les user grace à l'appel à la bdd

--- back/src/models/usersModels.js
+++ back/src/models/usersModels.js
@@ -1,7 +1,6 @@
 import pool from "./configDb.js";
 
 async function getAllUsersModel() {
-  console.log("vs code est bourré");
   const { rows } = await pool.query(`SELECT * FROM users`);
   console.log(rows);
   return rows;
