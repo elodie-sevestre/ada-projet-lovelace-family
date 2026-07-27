c9cf57b73bfcf307520f68c7dff839edbb0d3804
Offichigo <oph.perianin@gmail.com>
2026-07-27 19:22:09 +0200

    fix: ajout create task dans tasl consultation et ajout du refresh

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,27 +3,11 @@
 import TasksConsultation from "./components/TasksConsultation.jsx";
 
 import "./App.css";
-import TaskForm from "./components/TaskForm.jsx";
-import { createTask } from "./api/tasks.js";
-import { getUsers } from "./api/users.js";
-import { useEffect, useState } from "react";
-function App() {
-  const [members, setMembers] = useState([]);
 
-  useEffect(() => {
-    getUsers().then((result) => setMembers(result));
-  }, []);
-  console.log(members);
-  // const memberss = [
-  //   {
-  //     id: 1,
-  //     name: "Bernard",
-  //   },
-  // ];
+function App() {
   return (
     <>
       <section id="center">
-        <TaskForm onCreate={createTask} members={members} />
         <TasksConsultation />
       </section>
     </>
--- front/src/components/TasksConsultation.jsx
+++ front/src/components/TasksConsultation.jsx
@@ -1,21 +1,34 @@
 import TasksList from "./TasksList";
 import { useState, useEffect } from "react";
 import { getTasks } from "../api/tasks";
+import TaskForm from "./TaskForm";
+import { createTask } from "../api/tasks";
+import { getUsers } from "../api/users";
 
 function TasksConsultation() {
   const [tasks, setTasks] = useState({ toDoTasks: [], finishedTasks: [] });
   const currentUser = { role: "ADMIN" };
+  const [members, setMembers] = useState([]);
 
   const fetchTasks = () => {
     getTasks().then((result) => setTasks(result));
   };
 
   useEffect(() => {
+    getUsers().then((result) => setMembers(result));
+  }, []);
+
+  useEffect(() => {
     fetchTasks();
   }, []);
 
+  const onCreate = (taskToCreate) => {
+    createTask(taskToCreate).then(() => fetchTasks());
+  };
+
   return (
     <div className="tasks-consultation-contener">
+      <TaskForm onCreate={onCreate} members={members} />
       <div className="task-list-contener">
         <p className="tasks-list-title">A faire</p>
         <TasksList
