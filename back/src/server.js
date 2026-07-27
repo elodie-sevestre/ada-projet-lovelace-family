99b84947bc5fd4df0bde9e811f9d97b63b4a069c
Gaed <oger.gaedic@gmail.com>
2026-07-27 12:28:12 +0200

    chore: installation fichiers confiv env et variable d'environnement

--- back/src/server.js
+++ back/src/server.js
@@ -2,9 +2,10 @@ import express from "express";
 import cors from "cors";
 import usersRoutes from "./routes/usersRoutes.js";
 import tasksRoutes from "./routes/tasksRoutes.js";
+import { config } from "../config/env.js";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = config.port_back;
 
 app.use(cors());
 app.use(express.json());
