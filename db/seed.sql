918d86e4be4eeab0ff3b7b862ef2ecdc523003f7
Elodie S <elodie.sevestre86@gmail.com>
2026-07-28 12:36:08 +0200

    db: ajout attribut tribe_name + modif attribut password en password_hash

--- db/seed.sql
+++ db/seed.sql
@@ -2,9 +2,9 @@
 
 --Insérer des users :
 
-INSERT INTO users (role, name, mail, password, total_points, created_at, updated_at) VALUES
-  ('ADMIN', 'Bernard', 'bernard@aol.com', 'lemotdepasse', 0, NOW(), NOW()),
-  ('MEMBER', 'Léa', 'lillychat@gmail.fr', 'kawai3000', 0, NOW(), NOW());
+INSERT INTO users (role, name, mail, tribe_name, password_hash, total_points, created_at, updated_at) VALUES
+  ('ADMIN', 'Bernard', 'bernard@aol.com','la_tribu_de_bernard',  'lemotdepasse', 0, NOW(), NOW()),
+  ('MEMBER', 'Léa', 'lillychat@gmail.fr', 'la_tribu_de_bernard', 'kawai3000', 0, NOW(), NOW());
   
 --Insérer des tâches :
 INSERT INTO tasks (name, status, points, created_at, updated_at) VALUES
