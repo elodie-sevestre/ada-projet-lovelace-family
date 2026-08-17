c5a5fa20622a4ac8d9c21acebafa35f71c29478c
Gaed <oger.gaedic@gmail.com>
2026-08-17 15:43:53 +0200

    feat: Ajout Prettier en local config avec Eslint

--- back/eslint.config.js
+++ back/eslint.config.js
@@ -1,11 +1,12 @@
 export default [
   {
     rules: {
-      "no-unused-vars": "error", // variable déclarée mais jamais utilisée → erreur
-      "no-console": "warn", // console.log oublié → avertissement (pas bloquant)
-      "no-unreachable": "error", // code après un return → erreur
-      eqeqeq: ["error", "always"], // impose === au lieu de ==
-      "prefer-const": "error", // si une variable n'est jamais réassignée → const
+      'no-unused-vars': 'error', // variable déclarée mais jamais utilisée → erreur
+      'no-console': 'warn', // console.log oublié → avertissement (pas bloquant)
+      'no-unreachable': 'error', // code après un return → erreur
+      eqeqeq: ['error', 'always'], // impose === au lieu de ==
+      'prefer-const': 'error', // si une variable n'est jamais réassignée → const
     },
   },
+  prettier, //ajoute prettier dans la config eslint pour éviter les conflits entre les deux
 ];
