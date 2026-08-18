67f9f974f25863716f485aa293b90904e32914b7
Elodie S <elodie.sevestre86@gmail.com>
2026-08-18 16:04:22 +0200

    feait: ajout condition de validation des champs requis

--- back/src/controllers/loginControllers.js
+++ back/src/controllers/loginControllers.js
@@ -7,6 +7,10 @@ const loginController = async (req, res) => {
   try {
     const { role, name, mail, tribe_name, password } = req.body;
 
+    if (!name || !mail || !password) {
+      return res.statut(400).json({ error: "Champs requis manquants" });
+    }
+
     await createLoginService(role, name, mail, tribe_name, password);
 
     // On ne renvoie jamais le hash au client
