067258221a71a4a2e0d27e5fb6492cfa9288508c
Elodie S <elodie.sevestre86@gmail.com>
2026-08-24 13:34:52 +0200

    feat: ajout du formulaire de connexion et condition d'affichage avec composant TaskConsultation

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,14 +1,24 @@
-// import TaskItem from "./components/TaskItem.jsx";
-
+import { useState } from "react";
+import LoginForm from "./components/LoginForm.jsx";
 import TasksConsultation from "./components/TasksConsultation.jsx";
 
 import "./App.css";
 
 function App() {
+  // on récupère le token du localStorage
+  const [token, setToken] = useState(() => {
+    // console.log("Mon token : ", localStorage.getItem("token"));
+    return localStorage.getItem("token");
+  });
+
   return (
     <>
       <section id="center">
-        <TasksConsultation />
+        {token ? (
+          <TasksConsultation token={token} />
+        ) : (
+          <LoginForm setToken={setToken} />
+        )}
       </section>
     </>
   );
