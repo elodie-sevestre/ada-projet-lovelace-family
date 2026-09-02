115a081763888b3f431eab31c9d01564ebbbe011
Gaed <oger.gaedic@gmail.com>
2026-09-02 13:41:53 +0200

    feature: Ajout du logger et du errorHandler

---
+++ back/src/middlewares/errorHandler.js
@@ -0,0 +1,3 @@
+export function errorHandler(err, req, res, next) {}
+
+//A installer pour gérer les erreurs à la place du try and catch mettre une réponse avec le code de l'erreur. + logger les erreurs
--- back/src/scripts/migration_down.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import pool from '../models/configDb.js';
-
-const migrateDown = async () => {
-  const sql = fs.readFileSync('../db/migration_down.sql', 'utf8');
-  await pool.query(sql);
-};
-
-migrateDown();
