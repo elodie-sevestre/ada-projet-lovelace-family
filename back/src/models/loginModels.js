104d931f2568f4e52cb5aad85fb59f4ff31a5802
Elodie S <elodie.sevestre86@gmail.com>
2026-09-06 21:33:12 +0200

    refactor: retrait des try/catch car erreur gérée maintenant par errorHandler

--- back/src/models/loginModels.js
+++ back/src/models/loginModels.js
@@ -7,29 +7,19 @@ const createLoginModel = async (
   tribe_name,
   password_hash
 ) => {
-  try {
-    const { rows } = await pool.query(
-      'INSERT INTO users (role, name, mail, tribe_name, password_hash) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-      [role, name, mail, tribe_name, password_hash]
-    );
-    // console.log(rows);
-    return rows[0];
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
+  const { rows } = await pool.query(
+    'INSERT INTO users (role, name, mail, tribe_name, password_hash) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+    [role, name, mail, tribe_name, password_hash]
+  );
+  // console.log(rows);
+  return rows[0];
 };
 
 const findUserByEmail = async (mail) => {
-  try {
-    const { rows } = await pool.query('SELECT * FROM users WHERE mail = $1', [
-      mail,
-    ]);
-    return rows[0]; // undefined si aucun utilisateur trouvé
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
+  const { rows } = await pool.query('SELECT * FROM users WHERE mail = $1', [
+    mail,
+  ]);
+  return rows[0]; // undefined si aucun utilisateur trouvé
 };
 
 export { createLoginModel, findUserByEmail };
