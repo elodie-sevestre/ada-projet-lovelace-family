cd516f3befa637df67a83a81b78328b6e311c088
Elodie S <elodie.sevestre86@gmail.com>
2026-09-07 10:57:31 +0200

    chore: retire les scripts migration/seed JS inutilisés

--- back/src/scripts/seed.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import pool from '../models/configDb.js';
-
-const seed = async () => {
-  const sql = fs.readFileSync('../db/seed.sql', 'utf8');
-  await pool.query(sql);
-};
-
-seed();
